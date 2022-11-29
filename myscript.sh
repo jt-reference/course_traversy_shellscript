4664411ad83a5ff6279919ebf190c64b49f4747f
justintsugranes <justintsugranes@proton.me>
2022-11-29 11:53:29 -0500

    add tutorial notes

--- myscript.sh
+++ myscript.sh
@@ -15,7 +15,7 @@
 # VARIABLES
 # Variables are Uppercase by convention
 # What's allowed: letters, numbers, underscores
-NAME="James"
+# NAME="James"
 # echo "My name is $NAME"
 # or 
 # echo "My name is ${NAME}"
@@ -90,3 +90,75 @@ NAME="James"
 # fi
 
 # CASE STATEMENTS - like Switch Statements
+# prompt user - their answer is stored in a variable ANSWER
+# lowercase y or uppercase Y or any combination of yes (Yes, yEs, yeS, etc..) followed by a required closing parenthesis
+# echo out whatever
+# close out the case with a double semicolon
+# the no statement structure mirrors the above
+# all followed by an asterisk and closing parenthesis
+# lastly, the default (similar to the JS switch)
+# close it ou with case backwards (esac)
+# read -p "Are you 21 or over? Y/N " ANSWER
+# case "$ANSWER" in
+# [yY] | [yY][eE][sS])
+#   echo "You can have a beer :)"
+#   ;;
+# [nN] | [nN][oO])
+#   echo "Sorry, no drinking :("
+#   ;;
+#   *)
+#   echo "Please enter y/yes or n/no"
+#   ;;
+#   esac
+
+# SIMPLE 'FOR' LOOP
+# NAME defines the variable and $NAMES calls the variable
+# NAMES="Justin Brittany Jupiter"
+# for NAME in $NAMES
+#   do 
+#     echo "Hello ${NAME}"
+#   done
+
+# FOR LOOP TO RENAME FILES
+# list all of the text files. * stands for all
+# create a variable 'NEW' and set the new name to "new"
+# to execute the rename, use the 'mv' command
+# FILES=$(ls *.txt)
+# NEW="new"
+# for FILE in $FILES
+#   do
+#     echo "Renaming $FILE to new-$FILE"
+#     mv $FILE $NEW-$FILE
+# done
+
+# WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+# create a variable to stand as an iterator
+# ++ to increment
+# LINE=1
+# while read -r CURRENT_LINE
+#   do
+#     echo "$LINE: $CURRENT_LINE"
+#     ((LINE++))
+# done <"./new-1.txt"
+
+# FUNCTION
+# NAME="Justin"
+# function sayHello(){
+#   echo "Hello $NAME"
+# }
+# sayHello
+
+# FUNCTION WITH PARAMS
+# use positional parameters
+# function greet(){
+#   echo "Hello, I am $1 and I am $2 years old"
+# }
+
+# greet "Justin" "35"
+
+# #CREATE FOLDER AND WRITE TO A FILE
+# use >> to write to the file and then declare the file to write to
+# mkdir hello
+# touch "hello/world.txt"
+# echo "Hello World" >> "hello/world.txt"
+# echo "Create hello/world.txt"
